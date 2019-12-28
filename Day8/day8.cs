a7f76f7c8572dd13b09e90e83949120c9190414d
javiermolinar <javiermolinar@live.com>
2019-12-28 19:12:05 +0100

    Fixing changes

--- Day8/day8.cs
+++ Day8/day8.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -69,3 +70,44 @@ namespace ConsoleApp5
 
        
 }
+=======
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {                    
+            var myArray = File.ReadAllText(Directory.GetCurrentDirectory() + "\\input.txt").ToCharArray().Select(x=>x-'0').ToArray();   
+            
+            int sizeOfLayer = 150;                  
+            List<List<int>> myImage = new List<List<int>>();
+            List<int> myLayer = new List<int>();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                int pos = i % sizeOfLayer;
+                myLayer.Add(myArray[i]);
+
+                if(pos == sizeOfLayer - 1)
+                {
+                    myImage.Add(myLayer);
+                    myLayer = new List<int>();
+                }
+            }         
+          
+            int calculatedNumber = GetCalculatedValue(myImage.OrderBy(x=>x.Count(y=>y == 0)).FirstOrDefault());             
+
+            Console.WriteLine(calculatedNumber);    
+        }
+
+        static int GetCalculatedValue(List<int> layer) => layer.Count(x=>x==1) * layer.Count(x=>x==2);        
+
+    }
+}
+>>>>>>> 45df7474e36414b5cd489452c82f16885a5fc557
